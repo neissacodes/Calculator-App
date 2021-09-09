0773437ffd4ad9f647182c2b6c9d06f18b47f850
Neissa Dorsinville <neissa@dorsinville.com>
2021-09-08 22:42:54 -0700

    Test MultOperationTest.java added

--- src/test/java/com/example/demo/MultOperationTest.java
+++ src/test/java/com/example/demo/MultOperationTest.java
@@ -1,7 +1,21 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MultOperationTest {
+    private double num1;
+    private double num2;
+    private double num3;
+
+    @Test
+    public void mult_operationTest() {
+        MultOperation uut = new MultOperation();
+
+        double result = uut.multOperation(3,5,8);
+
+        assertEquals(result, 120);
+    }
 
 }
