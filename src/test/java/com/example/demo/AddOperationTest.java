0d38534a86b2a21610fb222cb0757f1ab6b2707c
Neissa Dorsinville <neissa@dorsinville.com>
2021-09-08 22:14:57 -0700

    Test AddOperationTest.java

---
+++ src/test/java/com/example/demo/AddOperationTest.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class AddOperationTest {
+   private double num1;
+   private double num2;
+   private double num3;
+   @Test
+   public void add_operationTest() {
+//      AddOperation uut = Mockito.mock(AddOperation.class);
+//
+//      double result = uut.addOperation(3,5,8);
+//
+//      Mockito.when(uut.addOperation(num1,num2,num3).thenReturn(16));
+//
+//      assertEquals(result, uut.addOperation(3,5,8));
+//   }
+      AddOperation uut = new AddOperation();
+
+      double result = uut.addOperation(3,5,8);
+
+      assertEquals(result, uut.addOperation(3,5,8));
+
+   }
+
+}
